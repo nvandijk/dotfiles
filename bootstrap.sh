e3beeed547ba21793c10a9f6bb8791f9c8172bb2
Niels van Dijk <info@nielsvandijk.nl>
2012-07-12 23:24:00 +0200

    updated boostrap file, symlink sublime text preferences and keymap files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+echo "symlink .gitconfig"
+rm ~/.gitconfig
 ln -s $(pwd)/.gitconfig ~/.gitconfig
+
+echo "symlink Sublime Text preferences and keymap"
+rm ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+ln -s $(pwd)/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+rm ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
+ln -s $(pwd)/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
