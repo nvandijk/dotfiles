807c3403f8c49e05b1caec6dcfa07368e3fa7922
Niels van Dijk <info@nielsvandijk.nl>
2012-07-16 18:16:55 +0200

    added gitignore dotfiles

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -3,3 +3,7 @@
 echo "symlink .gitconfig_linux"
 rm ~/.gitconfig
 ln -s $(pwd)/.gitconfig_linux ~/.gitconfig
+
+echo "symlink .gitignore_linux"
+rm ~/.gitignore
+ln -s $(pwd)/.gitignore_linux ~/.gitignore
--- bootstrap_osx.sh
+++ bootstrap_osx.sh
@@ -4,6 +4,10 @@ echo "symlink .gitconfig_osx"
 rm ~/.gitconfig
 ln -s $(pwd)/.gitconfig_osx ~/.gitconfig
 
+echo "symlink .gitignore_linux"
+rm ~/.gitignore
+ln -s $(pwd)/.gitignore_osx ~/.gitignore
+
 echo "symlink Sublime Text preferences and keymap"
 rm ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
 ln -s $(pwd)/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
