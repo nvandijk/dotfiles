93e70eb1c9b37cdcdef8903ab61e7b9b3d682cc1
Niels van Dijk <niels@nielsvandijk.nl>
2012-11-11 15:08:49 +0100

    added .bash_profile to init .bashrc for osx

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+# Load .bashrc
+source ~/.dotfiles/.bashrc
--- bootstrap_osx.sh
+++ bootstrap_osx.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 echo "symlink .dotfiles dir"
-rm ~/.dotfiles
+rm -f ~/.dotfiles
 ln -s $(pwd)/ ~/.dotfiles
 
-echo "symlink .bashrc"
-rm ~/.bashrc
-ln -s ~/.dotfiles/.bashrc ~/.bashrc
+echo "symlink .bash_profile"
+rm -f ~/.bash_profile
+ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 
 echo "symlink .gitconfig"
-rm ~/.gitconfig
+rm -f ~/.gitconfig
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
 echo "symlink .gitignore"
-rm ~/.gitignore
+rm -f ~/.gitignore
 ln -s ~/.dotfiles/.gitignore ~/.gitignore
 
 echo "symlink Sublime Text preferences and keymap"
-rm ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+rm -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
 ln -s ~/.dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
-rm ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
+rm -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
 ln -s ~/.dotfiles/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
