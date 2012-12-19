7f1a34a866d8c7d2bdef7df7005e9a2b36297e95
Niels van Dijk <niels@nielsvandijk.nl>
2012-12-19 23:28:19 +0100

    moved sublime text 2 files to folder, updated bootstrapper

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,13 +12,13 @@ echo "symlink .gitconfig"
 echo "symlink .gitignore"
 [ ! -f ~/.gitignore ] && ln -s ~/.dotfiles/.gitignore ~/.gitignore
 
-echo "symlink Sublime Text preferences and keymap"
-rm -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
-ln -s ~/.dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
-rm -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
-ln -s ~/.dotfiles/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
+echo "symlink Sublime Text 2 preferences"
+[ ! -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings ] && ln -s ~/.dotfiles/Sublime\ Text\ 2/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
 
-echo "symlink Sublime Text 2 command line"
+echo "symlink Sublime Text 2 keymap"
+[ ! -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap ] && ln -s ~/.dotfiles/Sublime\ Text\ 2/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
+
+echo "symlink Sublime Text 2 command line shortcut"
 [ ! -f /usr/local/bin/subl ] && ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 
 echo "setup os x settings"
