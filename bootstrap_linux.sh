2d60838cfae00a9eb79ad6f61654cc568b2ac8d1
Niels van Dijk <niels@nielsvandijk.nl>
2012-12-19 23:28:58 +0100

    updated linux bootstrapper

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
 echo "symlink .bash_profile"
-rm -f ~/.bash_profile
-ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
+[ ! -f ~/.bash_profile ] && ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 
 echo "symlink .screenrc"
-rm -f ~/.screenrc
-ln -s ~/.dotfiles/.screenrc ~/.screenrc
+[ ! -f ~/.screenrc ] && ln -s ~/.dotfiles/.screenrc ~/.screenrc
 
 echo "symlink .gitconfig"
-rm -f ~/.gitconfig
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+[ ! -f ~/.gitconfig ] && ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
 echo "symlink .gitignore"
-rm -f ~/.gitignore
-ln -s ~/.dotfiles/.gitignore ~/.gitignore
+[ ! -f ~/.gitignore ] && ln -s ~/.dotfiles/.gitignore ~/.gitignore
