1118556f9a76ec1341fd86f0aa6ff8aa8e97210e
Niels van Dijk <niels@nielsvandijk.nl>
2013-08-07 09:30:28 +0200

    Updated Sublime Text settings, moved Sublime Text User dir to .dotfiles project. Changed gitconfig and gitignore symlinking.

--- bootstrap_osx.sh
+++ bootstrap_osx.sh
@@ -4,18 +4,16 @@ echo "symlink osx .bash_profile"
 [ ! -f ~/.bash_profile ] && ln -s ~/.dotfiles/osx/.bash_profile ~/.bash_profile
 
 echo "symlink .gitconfig"
-[ ! -f ~/.gitconfig ] && ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+[ ! -f ~/.gitconfig ] && ln -s ~/.dotfiles/gitconfig.symlink ~/.gitconfig
 
 echo "symlink .gitignore"
-[ ! -f ~/.gitignore ] && ln -s ~/.dotfiles/.gitignore ~/.gitignore
+[ ! -f ~/.gitignore ] && ln -s ~/.dotfiles/gitignore.symlink ~/.gitignore
 
-echo "symlink Sublime Text 2 preferences"
-[ ! -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings ] && ln -s ~/.dotfiles/osx/Sublime\ Text\ 2/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+echo "symlink Sublime Text User dir"
+rm -r ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
+ln -s ~/.dotfiles/osx/Sublime\ Text/ ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
 
-echo "symlink Sublime Text 2 keymap"
-[ ! -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap ] && ln -s ~/.dotfiles/osx/Sublime\ Text\ 2/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
-
-echo "symlink Sublime Text 2 command line shortcut"
+echo "symlink Sublime Text command line shortcut"
 [ ! -f /usr/local/bin/subl ] && ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 
 echo "setup os x settings"
