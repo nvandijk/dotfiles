f477ea9140da11b9bcc23cd2b5a8136fcc52bccf
Niels van Dijk <niels@nielsvandijk.nl>
2015-07-27 11:33:48 +0200

    Removed homebrew cask instalation from bootsrap file to ansible.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,14 +8,6 @@ xcode-select --install
 echo "Installing Homebrew ..."
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install Homebrew cask
-echo "Installing Homebrew Cask ..."
-brew install caskroom/cask/brew-cask
-
-# Install Homebrew cask tap
-echo "Installing Homebrew Cask tap..."
-brew tap caskroom/versions
-
 # Modify the PATH
 export PATH=/usr/local/bin:$PATH
 
